3aa7e59b4bf779c5790788df8d9626277ce6247c
末心 <59173630+Moxin1044@users.noreply.github.com>
2023-05-02 20:39:20 +0800

    更新中小学生赛区错误和研究生赛区

--- GRD.py
+++ GRD.py
@@ -2,8 +2,7 @@ import requests
 import re
 from bs4 import BeautifulSoup
 import pandas as pd
-# 单独跑中小学生赛区
-# NYCC人不多，所以慢
+# 单独跑研究生赛区
 
 print("""
    ___      __     ___     ____  
@@ -38,7 +37,7 @@ for i in range(1, n + 1):
     h3_tag = soup.find('h3', {'class': 'text-center'})
     if h1_tag is not None:
         team_name = h1_tag.text
-        if team_name[:3] == "nycc-":
+        if team_name[:3] == "GRD-":
             h3_tag_text = h3_tag.text
             pattern = r'总积分为:(\d+),排在(\d+)位。'
             match = re.search(pattern, h3_tag_text)
@@ -60,10 +59,10 @@ df["排名"] = pd.to_numeric(df["排名"])
 # 按照排名排序
 df = df.sort_values("排名")
 # 将 DataFrame 写入 Excel 文件
-writer = pd.ExcelWriter("2023-ISCC-中小学赛区-练武题排行榜（排名顺序）.xlsx", engine="xlsxwriter")
+writer = pd.ExcelWriter("2023-ISCC-研究生赛区-练武题排行榜（排名顺序）.xlsx", engine="xlsxwriter")
 df.to_excel(writer, index=False, sheet_name="Teams")
 writer.close()
-print("【注意】：2023-ISCC--中小学赛区-练武题排行榜（排名顺序）.xlsx 已生成")
+print("【注意】：2023-ISCC-研究生赛区-练武题排行榜（排名顺序）.xlsx 已生成")
 # 按照ID的方式保存为xlsx
 df = pd.DataFrame(rlist)
 # 将排名列转换为数字类型
@@ -71,10 +70,10 @@ df["ID"] = pd.to_numeric(df["ID"])
 # 按照排名排序
 df = df.sort_values("ID")
 # 将 DataFrame 写入 Excel 文件
-writer = pd.ExcelWriter("2023-ISCC-中小学赛区-练武题排行榜（ID顺序）.xlsx", engine="xlsxwriter")
+writer = pd.ExcelWriter("2023-ISCC-研究生赛区-练武题排行榜（ID顺序）.xlsx", engine="xlsxwriter")
 df.to_excel(writer, index=False, sheet_name="Teams")
 writer.close()
-print("【注意】：2023-ISCC-中小学赛区-练武题排行榜（ID顺序）.xlsx 已生成")
+print("【注意】：2023-ISCC-研究生赛区-练武题排行榜（ID顺序）.xlsx 已生成")
 print("【注意】：练武排行榜已生成，正在生成擂台赛排行榜！")
 
 leitai_url_template = "https://iscc.isclab.org.cn/teamarena/{}" # 擂台赛接口
@@ -88,7 +87,7 @@ for i in range(1, n + 1):
     h3_tag = soup.find('h3', {'class': 'text-center'})
     if h1_tag is not None:
         team_name = h1_tag.text
-        if team_name[:3] == "nycc-":
+        if team_name[:3] == "GRD-":
             h3_tag_text = h3_tag.text
             pattern = r'总积分为:(\d+),排在(\d+)位。'
             match = re.search(pattern, h3_tag_text)
@@ -111,10 +110,10 @@ df["排名"] = pd.to_numeric(df["排名"])
 # 按照排名排序
 df = df.sort_values("排名")
 # 将 DataFrame 写入 Excel 文件
-writer = pd.ExcelWriter("2023-ISCC-中小学赛区-擂台赛排行榜（排名顺序）.xlsx", engine="xlsxwriter")
+writer = pd.ExcelWriter("2023-ISCC-研究生赛区-擂台赛排行榜（排名顺序）.xlsx", engine="xlsxwriter")
 df.to_excel(writer, index=False, sheet_name="Teams")
 writer.close()
-print("【注意】：2023-ISCC-中小学赛区-擂台赛排行榜（排名顺序）.xlsx 已生成")
+print("【注意】：2023-ISCC-研究生赛区-擂台赛排行榜（排名顺序）.xlsx 已生成")
 # 按照ID的方式保存为xlsx
 df = pd.DataFrame(r1list)
 # 将排名列转换为数字类型
@@ -122,8 +121,8 @@ df["ID"] = pd.to_numeric(df["ID"])
 # 按照排名排序
 df = df.sort_values("ID")
 # 将 DataFrame 写入 Excel 文件
-writer = pd.ExcelWriter("2023-ISCC-中小学赛区-擂台赛排行榜（ID顺序）.xlsx", engine="xlsxwriter")
+writer = pd.ExcelWriter("2023-ISCC-研究生赛区-擂台赛排行榜（ID顺序）.xlsx", engine="xlsxwriter")
 df.to_excel(writer, index=False, sheet_name="Teams")
 writer.close()
-print("【注意】：2023-ISCC-中小学赛区-擂台赛排行榜（ID顺序）.xlsx 已生成")
+print("【注意】：2023-ISCC-研究生赛区-擂台赛排行榜（ID顺序）.xlsx 已生成")
 print("【注意】：已全部完成！")
